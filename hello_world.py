2eec37f10672610e7ecf8c69b6bcc7143939ecbe
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2022-07-17 01:15:46 +0400

    templates

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/99-bottles')
+def get_bottles():
+    return render_template('bottles.html', bottles=range(99, 0, -1))
+
