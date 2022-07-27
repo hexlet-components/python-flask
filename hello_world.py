ef8735b33ee57a0e948ff8073275178a8b889968
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2022-07-27 14:40:18 +0400

    add navigation links at main page

--- hello_world.py
+++ hello_world.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html', greeting='Hello World!')
 
 
 @app.route('/99-bottles')
